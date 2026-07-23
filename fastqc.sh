c52151c6418ed1d4dff1c21592004a67fd247ef4
sebas-axl94 <sebashv94@gmail.com>
2026-07-23 18:21:15 -0500

    Modify fastqc.sh script (change of languaje and directories reorder) and addition of hisat2.sh script

--- fastqc.sh
+++ fastqc.sh
@@ -2,13 +2,13 @@
 
 set -euo pipefail
 
-mkdir -p ./reportes/fastqc_pre
-mkdir -p ./secuencias_trimadas
-mkdir -p ./reportes/fastqc_post
+mkdir -p ./reports/fastqc_pre
+mkdir -p ./trimmed_seqs
+mkdir -p ./reports/fastqc_post
 
-echo "=============================================="
-echo "Inicio, procesamiento con Fastqc y Trimmomatic"
-echo "=============================================="
+echo "==============================================="
+echo "~~~~~~~Fastqc and Trimmomatic processing~~~~~~~"
+echo "==============================================="
 for condition in HBR UHR
 do
    for replica in 1 2 3
@@ -17,48 +17,48 @@ do
 
      if [ "$MODO" == "single" ]; then
 
-       #Analisis con Fastqc antes de trimar
-        echo "Analisis fastqc de ${sample} pre-procesamiento"
-        fastqc ./reads/${sample}.fq.gz -o ./reportes/fastqc_pre
+       #Fastqc analysis prior trimming
+        echo "${sample} FASTQC analysis before trimming"
+        fastqc ./reads/${sample}.fq.gz -o ./reports/fastqc_pre
 
-       #Trimado de secuencias
-        echo "Limpieza de secuencias"
+       #Trimming
+        echo "Sequence Trimming"
         TrimmomaticSE ./reads/${sample}.fq.gz \
-         ./secuencias_trimadas/${sample}_trimada.fq.gz \
+         ./trimmed_seqs/${sample}_trimmed.fq.gz \
          SLIDINGWINDOW:4:30 \
          MINLEN:50
          #ILLUMINACLIP:nextera.fa:2:30:5
 
-       #Analisis con Fastqc despues de trimar
-        echo "Analisis fastqc de ${sample} post-procesamiento"
-        fastqc ./secuencias_trimadas/${sample}_trimada.fq.gz  -o ./reportes/fastqc_post
+       #Fastqc analysis after trimming
+        echo "${sample} FASTQC analysis after trimming"
+        fastqc ./trimmed_seqs/${sample}_trimmed.fq.gz  -o ./reports/fastqc_post
 
      else 
-       #Directorio para unpaired:
-       mkdir -p ./secuencias_trimadas_unpaired
+       #Unpaired sequences directory:
+       mkdir -p ./trimmed_seqs_unpaired
 
-       #Analisis con Fastqc antes de trimar
-        echo "Analisis fastqc de ${sample}_R1 y ${sample}_R2 pre-procesamiento"
-        fastqc ./reads/${sample}_R1.fq.gz ./reads/${sample}_R2.fq.gz -o ./reportes/fastqc_pre
+       #Fastqc analysis prior trimming
+        echo "${sample}_R1 and ${sample}_R2 FASTQC analysis before trimming"
+        fastqc ./reads/${sample}_R1.fq.gz ./reads/${sample}_R2.fq.gz -o ./reports/fastqc_pre
 
-       #Trimado de secuencias
-        echo "Limpieza de secuencias"
+       #Trimming
+        echo "Sequence Trimming"
         TrimmomaticPE ./reads/${sample}_R1.fq.gz ./reads/${sample}_R2.fq.gz \
-         ./secuencias_trimadas/${sample}_R1_trimada.fq.gz ./secuencias_trimadas_unpaired/${sample}_R1_trimada_unpaired.fq \
-         ./secuencias_trimadas/${sample}_R2_trimada.fq.gz ./secuencias_trimadas_unpaired/${sample}_R2_trimada_unpaired.fq \
+         ./trimmed_seqs/${sample}_R1_trimmed.fq.gz ./trimmed_seqs_unpaired/${sample}_R1_trimmed_unpaired.fq.gz \
+         ./trimmed_seqs/${sample}_R2_trimmed.fq.gz ./trimmed_seqs_unpaired/${sample}_R2_trimmed_unpaired.fq.gz \
          SLIDINGWINDOW:4:30 \
          MINLEN:50
          #ILLUMINACLIP:nextera.fa:2:30:5
 
-        #Analisis con Fastqc despues de trimar
-        echo "Analisis fastqc de ${sample}_R1 y ${sample}_R2 post-procesamiento"
-        fastqc ./secuencias_trimadas/${sample}_R1_trimada.fq.gz ./secuencias_trimadas/${sample}_R2_trimada.fq.gz -o ./reportes/fastqc_post
+        #Fastqc analysis after trimming
+        echo "${sample}_R1 and ${sample}_R2 FASTQC analysis before trimming"
+        fastqc ./trimmed_seqs/${sample}_R1_trimmed.fq.gz ./trimmed_seqs/${sample}_R2_trimmed.fq.gz -o ./reports/fastqc_post
      fi
    done
 done
 
 
-echo "=============================================="
-echo "Fin, procesamiento con Fastqc y Trimmomatic"
-echo "=============================================="
+echo "==============================================="
+echo "~~~~~~~Analysis and Processing Completed~~~~~~~"
+echo "==============================================="
 
---
+++ hisat2.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function bam_conversor {
+   samtools sort -o ./reports/bam/${sample}.bam ./reports/sam/${sample}.sam
+   rm ./reports/sam/${sample}.sam
+ return
+}
+
+function bam_index {
+   samtools index ./reports/bam/${sample}.bam
+ return
+}
+
+mkdir -p ./reports/sam 
+mkdir -p ./reports/bam
+
+echo "===================================================="
+echo "~~~~~~~~~Reference genome index with HISAT2~~~~~~~~~"
+echo "===================================================="
+
+#For splicing sites
+echo "Extraction splice sites from the reference genome file"
+hisat2_extract_splice_sites.py ./refs/*.gtf > ./refs/file.ss
+
+echo "Extraction exon sites from the reference genome file"
+#For exon sites
+hisat2_extract_exons.py ./refs/*.gtf > ./refs/file.exons
+
+#Genome index
+# -p: threads | --ss: splice sites | --exon: exon sites | reference genome | index prefix
+hisat2-build \
+ -p 4 \
+ --ss ./refs/file.ss \
+ --exon ./refs/file.exons \
+ ./refs/*.fa \
+ ./refs/index
+
+
+echo "====================================================="
+echo "~Aligning reads to the reference genome using HISAT2~"
+echo "====================================================="
+
+for condition in HBR UHR
+ do
+  for replica in 1 2 3 
+   do
+    sample="${condition}_${replica}"
+
+    if [ "$MODO" == "single" ]; then
+
+       echo "========================================================"
+       echo "~~~~~~~~~~~~~~~~~~~~Aligning $sample~~~~~~~~~~~~~~~~~~~~"
+       echo "========================================================"
+
+       hisat2 \
+         -p 4 \
+         -x ./refs/index \
+         -U ./trimmed_seqs/${sample}_trimmed.fq.gz \
+         -S ./reports/sam/${sample}.sam
+
+       echo "=========================================================="
+       echo "~~~~Converting $sample.sam to $sample.bam and indexing~~~~"
+       echo "=========================================================="
+
+       bam_conversor
+       bam_index
+
+    else
+
+       echo "========================================================"
+       echo "~~~~~~~~~~~~~~~~~~~~~Aligning $sample~~~~~~~~~~~~~~~~~~~"
+       echo "========================================================"
+
+       hisat2 \
+         -p 4 \
+         -x ./refs/index \
+         -1 ./trimmed_seqs/${sample}_R1_trimmed.fq.gz \
+         -2 ./trimmed_seqs/${sample}_R2_trimmed.fq.gz \
+         -S ./reports/sam/${sample}.sam
+
+       echo "=========================================================="
+       echo "~~~~Converting $sample.sam to $sample.bam and indexing~~~~"
+       echo "=========================================================="
+
+       bam_conversor
+       bam_index
+
+    fi
+   done
+done   
+
+echo "========================================================="
+echo "~~~~~~~~~~~~~~~~~~~~Analysis Completed~~~~~~~~~~~~~~~~~~~"
+echo "========================================================="
+
