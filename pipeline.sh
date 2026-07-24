6ac82183a162471188dce04552af342a099ed4b4
sebas-axl94 <sebashv94@gmail.com>
2026-07-24 18:26:44 -0500

    Add the pipeline.sh script (main script)

---
+++ pipeline.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail
+
+
+echo "Select the type of Reads: 
+1. Single End
+2. Paired End
+3. Exit"
+read -p "Option :" option
+
+case $option in
+   1) MODO="single" ;;
+   2) MODO="paired" ;;
+   3) echo "Exit..."; exit 0 ;;
+   *) echo "Invalid option"; exit 1 ;;
+esac
+
+#Export of MODO variable  according to the type of reads selected
+export MODO
+
+#Run the scripts
+bash ./fastqc.sh
+bash ./hisat2.sh
+bash ./featurecounts.sh
