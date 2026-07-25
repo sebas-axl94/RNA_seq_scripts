b0a0fb27a8b4f25e8aa3f37c3a43111011f49288
sebas-axl94 <sebashv94@gmail.com>
2026-07-24 19:09:30 -0500

    Organize sub-scripts into the scripts/ directory

---
+++ scripts/fastqc.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -euo pipefail
+
+mkdir -p ./reports/fastqc_pre
+mkdir -p ./trimmed_seqs
+mkdir -p ./reports/fastqc_post
+
+echo "==============================================="
+echo "~~~~~~~Fastqc and Trimmomatic processing~~~~~~~"
+echo "==============================================="
+
+for condition in HBR UHR
+do
+   for replica in 1 2 3
+   do
+     sample="${condition}_${replica}"
+
+     if [ "$MODO" == "single" ]; then
+
+       #Fastqc analysis prior trimming
+        echo "${sample} FASTQC analysis before trimming"
+        fastqc ./reads/${sample}.fq.gz -o ./reports/fastqc_pre
+
+       #Trimming
+        echo "Sequence Trimming"
+        TrimmomaticSE ./reads/${sample}.fq.gz \
+         ./trimmed_seqs/${sample}_trimmed.fq.gz \
+         SLIDINGWINDOW:4:30 \
+         MINLEN:50
+         #ILLUMINACLIP:nextera.fa:2:30:5
+
+       #Fastqc analysis after trimming
+        echo "${sample} FASTQC analysis after trimming"
+        fastqc ./trimmed_seqs/${sample}_trimmed.fq.gz  -o ./reports/fastqc_post
+
+     else 
+       #Unpaired sequences directory:
+       mkdir -p ./trimmed_seqs_unpaired
+
+       #Fastqc analysis prior trimming
+        echo "${sample}_R1 and ${sample}_R2 FASTQC analysis before trimming"
+        fastqc ./reads/${sample}_R1.fq.gz ./reads/${sample}_R2.fq.gz -o ./reports/fastqc_pre
+
+       #Trimming
+        echo "Sequence Trimming"
+        TrimmomaticPE ./reads/${sample}_R1.fq.gz ./reads/${sample}_R2.fq.gz \
+         ./trimmed_seqs/${sample}_R1_trimmed.fq.gz ./trimmed_seqs_unpaired/${sample}_R1_trimmed_unpaired.fq.gz \
+         ./trimmed_seqs/${sample}_R2_trimmed.fq.gz ./trimmed_seqs_unpaired/${sample}_R2_trimmed_unpaired.fq.gz \
+         SLIDINGWINDOW:4:30 \
+         MINLEN:50
+         #ILLUMINACLIP:nextera.fa:2:30:5
+
+        #Fastqc analysis after trimming
+        echo "${sample}_R1 and ${sample}_R2 FASTQC analysis before trimming"
+        fastqc ./trimmed_seqs/${sample}_R1_trimmed.fq.gz ./trimmed_seqs/${sample}_R2_trimmed.fq.gz -o ./reports/fastqc_post
+     fi
+   done
+done
+
+
+echo "==============================================="
+echo "~~~~~~~Analysis and Processing Completed~~~~~~~"
+echo "==============================================="
+
---
+++ scripts/featurecounts.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+set -euo pipefail 
+
+mkdir -p ./counts
+
+ 
+function cleaner {
+ sed "1d" ./counts/counts_matrix.txt | cut -f 1,7-12 | tr '\t' ',' | sed "1s/\.\/reports\/bam\///g" | sed "1s/\.bam//g" > ./counts/counts_matrix.csv
+return
+}
+
+function visualizer {
+ column -t -s ',' ./counts/counts_matrix.csv | head -n 15 || true
+return
+}
+
+
+
+if [ "$MODO" == "single" ]; then
+   
+     echo "Counts matrix for single-end"
+     featureCounts -a ./refs/*.gtf -t exon -g gene_id -o ./counts/counts_matrix.txt ./reports/bam/*.bam
+
+     echo "Cleaning matrix and transform to .csv"
+     cleaner
+
+     echo "============================================================"
+     echo "~~~~~~~~~~~~~~~~~~~~~~~Preview matrix~~~~~~~~~~~~~~~~~~~~~~~"
+     echo "============================================================"
+     visualizer
+
+ else
+
+     echo "Counts matrix for paired-end"
+     featureCounts -p -a ./refs/*.gtf -t exon -g gene_id -o ./counts/counts_matrix.txt ./reports/bam/*.bam
+
+     echo "Cleaning matrix and transform to .csv"
+     cleaner
+
+     echo "============================================================"
+     echo "~~~~~~~~~~~~~~~~~~~~~~~Preview matrix~~~~~~~~~~~~~~~~~~~~~~~"
+     echo "============================================================"
+     visualizer
+fi
+
+
+#Experimental design file
+     echo "============================================================="
+     echo "~~~~~~~~~~~~~Experimental design file generation~~~~~~~~~~~~~"
+     echo "============================================================="
+mkdir -p ./metadata
+design_file=./metadata/design.csv
+
+echo "sample,condition" > $design_file
+
+for condition in HBR UHR
+ do
+   for replica in 1 2 3
+   do
+     sample="${condition}_${replica}"
+     echo "${sample},${condition}" >> $design_file
+   done
+ done
+
+echo "Experimental design file generated in: /metadata"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
